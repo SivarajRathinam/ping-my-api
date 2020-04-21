4106d33ba76e878262d3df076ce7fa75c131e042
SivarajRathinam <sivaraj.mca17@gmail.com>
2020-04-22 00:29:39 +0530

    Updates

---
+++ precache-manifest.62de6c6dbafa661aa8904df6c995e801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be22d1c4a85a7ede1ee90cd66b2487aa",
+    "url": "/ping-my-api/index.html"
+  },
+  {
+    "revision": "00826a5b75adabe1046c",
+    "url": "/ping-my-api/static/css/main.d6c516a3.chunk.css"
+  },
+  {
+    "revision": "970531147d27c0cbf2fd",
+    "url": "/ping-my-api/static/js/2.b51cc017.chunk.js"
+  },
+  {
+    "revision": "d26cf3db69a09796eb6a71ff1d1c75cc",
+    "url": "/ping-my-api/static/js/2.b51cc017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00826a5b75adabe1046c",
+    "url": "/ping-my-api/static/js/main.de0853aa.chunk.js"
+  },
+  {
+    "revision": "95c68139b78c0aea997a",
+    "url": "/ping-my-api/static/js/runtime-main.3da4610b.js"
+  },
+  {
+    "revision": "71077684efb1c8653ca6ff05f7909e82",
+    "url": "/ping-my-api/static/media/close.71077684.png"
+  }
+]);
